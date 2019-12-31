83d4299f6e2a1dc78ab955267c5a60e83b7ac90b
priesto <antoveccio@gmail.com>
2019-12-31 14:57:33 +0100

    add historized result

--- recup.py
+++ recup.py
@@ -1,11 +1,12 @@
 import json
 import time
 import serial
+import os
 
 info = {}
 
 try:
-	with open('/home/pi/aqn-ceri/i2c/result','r') as f:
+	with open('/home/pi/Desktop/co2','r') as f:
 		txt = f.read().split(',')
 		info['co2'] = txt[0]
 		info['cov'] = txt[1].split('\n')[0]
@@ -14,7 +15,7 @@ except ValueError:
 	raise
 
 try:
-	with open('/home/pi/aqn-ceri/dht/result','r') as f:
+	with open('/home/pi/Desktop/dht/dht_log.txt','r') as f:
 		txt = f.read().split(',')
 		info['temp'] = txt[0]
 		info['humid'] = txt[1].split('\n')[0]
@@ -23,11 +24,28 @@ except ValueError:
 	raise
 
 try:
-	with open('/home/pi/aqn-ceri/result','w') as f:
+	with open('/home/pi/Desktop/result.json','w') as f:
 		f.write(json.dumps(info))
 except ValueError:
 	print('Error open file !')
 	raise
+
+
+
+if not os.path.isfile('/home/pi/Desktop/result.hist.json'):
+	with open('/home/pi/Desktop/result.hist.json', 'w+') as f:
+		data = []
+		data.append(info)
+		f.write(json.dumps(data))
+
+else:
+	with open('/home/pi/Desktop/result.hist.json', 'r') as f:
+		data = json.load(f)
+		data.append(info)
+
+	with open('/home/pi/Desktop/result.hist.json', 'w') as f:
+		f.write(json.dumps(data))
+
 
 port = "/dev/ttyACM0"
 
@@ -36,5 +54,5 @@ s1.flushInput()
 
 s1.write(json.dumps(info))
 
-time.sleep(5)
 
+time.sleep(5)
